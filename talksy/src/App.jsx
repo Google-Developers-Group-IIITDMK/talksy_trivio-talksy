0f5ab32685a4d770804d759892ec67f8533baa9c
AnilKumt <anilkumt51@gmail.com>
2025-09-27 14:27:28 +0530

    corrected navbar positioning

--- talksy/src/App.jsx
+++ talksy/src/App.jsx
@@ -49,12 +49,12 @@ const App = () => {
     <>
       <div
         id="nav"
-        className="flex justify-between items-center w-full h-24 fixed px-10 z-99"
+        className="flex justify-between items-center w-full h-24 fixed px-10 py-10 z-99 "
       >
         <h3 className="font-[gilroy] font-400 text-5xl cursor-pointer">
           TALKSY
         </h3>
-        <button className="px-5 py-5 bg-black text-white text-4xl border-none font-[gilroy] rounded-4xl mt-0.5 cursor-pointer">
+        <button className="px-5 py-5 bg-black text-white text-4xl border-none font-[gilroy] rounded-4xl cursor-pointer">
           TRY NOW
         </button>
       </div>
